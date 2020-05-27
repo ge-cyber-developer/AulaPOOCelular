e783aaca4cb6674e8b272268c01b0e1d413b559f
ge-cyber-developer <61585687+ge-cyber-developer@users.noreply.github.com>
2020-05-27 11:33:19 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace celular
+{
+    class celular
+    {
+        public string cor;
+        public string modelo;
+        public string tamanho;
+        public bool ligado = true;
+        public celular(string _cor, string _modelo, string _tamanho){
+            this.cor = _cor;
+            this.modelo = _modelo;
+            this.tamanho = _tamanho;
+        }
+
+        public void ligar(){
+            this.ligado = true;
+        }
+
+        public void desligar(){
+            this.ligado = false;
+        }
+
+        public void fazendoLigacao(string pessoa){
+            Console.WriteLine("Fazendo ligação para: "+pessoa);
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            celular celularDaGeorgia = new celular("verde neon","iphone 11", "grande");
+            Console.WriteLine(celularDaGeorgia.cor);
+        }
+    }
+}
